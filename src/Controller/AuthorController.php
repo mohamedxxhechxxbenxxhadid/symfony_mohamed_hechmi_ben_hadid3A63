de07c64b1fa9ba22d042330e833ab0e400032c31
3LHECH <mohamedhehcmi.benhadid@gmail.com>
2023-10-07 22:32:13 +0100

    first commit

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+    #[Route('/showAuthor/{name}', name: 'app_ShowAuthor')]
+    public function showAuthor($name): Response
+    {
+        return $this->render('author/show.html.twig',['n'=>$name ]);
+    }
+    #[Route('/showList', name: 'app_ShowList')]
+    public function showList(): Response
+    {
+        $authors = array(
+            array('id' => 1, 'picture' => '/images/victor-hugo.jpg','username' => 'Victor Hugo', 'email' =>
+            'victor.hugo@gmail.com ', 'nb_books' => 0),
+            array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
+            ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
+            array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
+            'taha.hussein@gmail.com', 'nb_books' => 300),
+            );
+        return $this->render('author/list.html.twig',['authors'=>$authors]);
+    }
+    #[Route('/showdetails/{id}', name: 'app_ShowDetails')]
+    public function authorDetails($id): Response
+    {
+        $authors = array(
+            array('id' => 1, 'picture' => '/images/victor-hugo.jpg','username' => 'Victor Hugo', 'email' =>
+            'victor.hugo@gmail.com ', 'nb_books' => 0),
+            array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
+            ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
+            array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
+            'taha.hussein@gmail.com', 'nb_books' => 300),
+            );
+        return $this->render('author/showAuthor.html.twig',['author'=>$authors[$id-1]]);
+    }
+}
